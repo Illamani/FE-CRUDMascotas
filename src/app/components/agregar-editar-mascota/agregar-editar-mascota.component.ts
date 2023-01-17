efecc812ace9f6b7f05c901f82deaab77d3c0bb1
WB Vera <WBVera@rosariobus.com.ar>
2023-01-17 11:04:41 -0300

    Implementacion Formulario Reactivo y objeto pre Backend

--- src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
+++ src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Mascota } from 'src/app/interfaces/mascotas';
 
 @Component({
   selector: 'app-agregar-editar-mascota',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-editar-mascota.component.css']
 })
 export class AgregarEditarMascotaComponent implements OnInit {
-  loading: boolean = false
-  constructor() { }
+  loading: boolean = false;
+
+  form: FormGroup
+  constructor(private fb: FormBuilder) { 
+    this.form = this.fb.group({
+      nombre:['',Validators.required],
+      raza:['',Validators.required],
+      color:['',Validators.required],
+      edad:['',Validators.required],
+      peso:['',Validators.required],
+    })
+  }
 
   ngOnInit(): void {
+  }
+  agregarMascota(){
+    const nombre = this.form.value.nombre;
+    console.log(nombre);
+
+    const mascota : Mascota = {
+      nombre : this.form.value.nombre,
+      color : this.form.value.color,
+      edad : this.form.value.edad,
+      peso : this.form.value.peso,
+      raza : this.form.value.raza,
+    }
+    console.log(mascota);
   }
 
 }
