c7f765241926d89d1d308168c60bd2eb4fca72fe
WB Vera <WBVera@rosariobus.com.ar>
2023-01-22 10:52:37 -0300

    Insert BackEnd

---
+++ BE-CRUDMascotas/BE-CRUDMascotas/Models/Mascotas.cs
@@ -0,0 +1,12 @@
+namespace BE_CRUDMascotas.Models
+{
+  public class Mascotas
+  {
+    public int Id { get; set; }
+    public string Raza { get; set; }
+    public string Color { get; set; }
+    public int Edad { get; set; }
+    public float Peso { get; set; }
+    public DateTime FechaCreacion { get; set; }
+  }
+}
--- BE-CRUDMascotas/BE-CRUDMascotas/Program.cs
+++ BE-CRUDMascotas/BE-CRUDMascotas/Program.cs
@@ -1,3 +1,6 @@
+using BE_CRUDMascotas.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var conecction = builder.Configuration.GetConnectionString("Conexion");
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+{
+  options.UseSqlServer(conecction);
+});
 
 var app = builder.Build();
 
