9d8b595f54ce50226f9c3924ae8e9145cfb40468
WB Vera <WBVera@rosariobus.com.ar>
2023-01-22 13:21:54 -0300

    BuscarMascota

--- BE-CRUDMascotas/BE-CRUDMascotas/Controllers/MascotasController.cs
+++ BE-CRUDMascotas/BE-CRUDMascotas/Controllers/MascotasController.cs
@@ -20,13 +20,27 @@ namespace BE_CRUDMascotas.Controllers
     {
       try
       {
-        Thread.Sleep(2000);
         var listMascotas = await _context.Mascotas.ToListAsync();
         return Ok(listMascotas);
       }
       catch (Exception ex)
       {
         return BadRequest(ex.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+      try
+      {
+        var mascota = await _context.Mascotas.FindAsync(id);
+        if (mascota == null) { return NotFound(); }
+        return Ok(mascota);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest($"{ex.Message}");
       }
     }
   }
